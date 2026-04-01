2445627877d668734a40203821d7eb7e411347ba
TOBIAS ISAAC EPSZTEIN <49194388@ort>
2026-04-01 09:17:39 -0300

    CSS TERMINADO

--- src/Cita.jsx
+++ src/Cita.jsx
@@ -3,34 +3,17 @@ import "./App.css";
 function Cita({ mascota, dueno, fecha, hora, sintomas }) {
   return (
     <div class="cita-card">
-      <div class="cita-header">
-        <div class="mascota-nombre">{mascota}</div>
-        <button class="boton-eliminar">
-          ELIMINAR
-        </button>
+
+      <div class="cita-contenido">
+        <p><span>Mascota:</span>{mascota}</p>
+        <p><span>Dueño:</span> {dueno}</p>
+        <p><span>Fecha:</span> {fecha}</p>
+        <p><span>Hora:</span> {hora}</p>
+        <p><span>Síntomas:</span> {sintomas}</p>
       </div>
-      
-      <div class="cita-detalles">
-        <div class="detalle-item">
-          <span class="detalle1">Dueño:</span>
-          <span class="detalle2">{dueno}</span>
-        </div>
-        
-        <div class="detalle-item">
-          <span class="detalle1">Fecha:</span>
-          <span class="detalle2">{fecha}</span>
-        </div>
-        
-        <div class="detalle-item">
-          <span class="detalle1">Hora:</span>
-          <span class="detalle2">{hora}</span>
-        </div>
-        
-        <div class="sintomas-texto">
-          <span class="detalle1">Síntomas:</span>
-          <span class="detalle2">{sintomas}</span>
-        </div>
-      </div>
+
+      <button className="boton-eliminar">ELIMINAR X</button>
+
     </div>
   );
 }
